d2052ecb5c0eeba74404b0037974304431754a61
bloeys <bloeys@outlook.com>
2022-06-30 06:38:54 +0400

    Running window

--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/bloeys/nterm
 
 go 1.18
+
+require github.com/bloeys/nmage v0.11.12
+
+require (
+	github.com/bloeys/assimp-go v0.4.2 // indirect
+	github.com/bloeys/gglm v0.3.1 // indirect
+	github.com/go-gl/gl v0.0.0-20211025173605-bda47ffaa784 // indirect
+	github.com/inkyblackness/imgui-go/v4 v4.3.0 // indirect
+	github.com/veandco/go-sdl2 v0.4.10 // indirect
+)
--- main.go
+++ main.go
@@ -1,5 +1,76 @@
 package main
 
+import (
+	"github.com/bloeys/nmage/engine"
+	"github.com/bloeys/nmage/input"
+	"github.com/bloeys/nmage/renderer/rend3dgl"
+	nmageimgui "github.com/bloeys/nmage/ui/imgui"
+)
+
+var _ engine.Game = &program{}
+
+type program struct {
+	shouldRun bool
+	win       *engine.Window
+	imguiInfo nmageimgui.ImguiInfo
+}
+
+func (p *program) Init() {
+
+}
+
+func (p *program) Start() {
+
+}
+
+func (p *program) FrameStart() {
+
+}
+
+func (p *program) Update() {
+
+	if input.IsQuitClicked() {
+		p.shouldRun = false
+	}
+
+}
+
+func (p *program) Render() {
+
+}
+
+func (p *program) FrameEnd() {
+
+}
+
+func (g *program) GetWindow() *engine.Window {
+	return g.win
+}
+
+func (g *program) GetImGUI() nmageimgui.ImguiInfo {
+	return g.imguiInfo
+}
+
+func (p *program) ShouldRun() bool {
+	return p.shouldRun
+}
+
+func (p *program) Deinit() {
+
+}
+
 func main() {
-	println("Hi!")
+
+	win, err := engine.CreateOpenGLWindowCentered("nTerm", 1280, 720, engine.WindowFlags_ALLOW_HIGHDPI|engine.WindowFlags_RESIZABLE, rend3dgl.NewRend3DGL())
+	if err != nil {
+		panic("Failed to create window. Err: " + err.Error())
+	}
+
+	p := &program{
+		shouldRun: true,
+		win:       win,
+		imguiInfo: nmageimgui.NewImGUI(),
+	}
+
+	engine.Run(p)
 }
