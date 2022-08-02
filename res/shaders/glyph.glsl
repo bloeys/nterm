d296fb2dd6a827b882a87cb20fb32f0be691e8e7
bloeys <bloeys@outlook.com>
2022-08-02 05:45:14 +0400

    Simplify glyph shader

--- res/shaders/glyph.glsl
+++ res/shaders/glyph.glsl
@@ -44,12 +44,8 @@ in vec4 gl_FragCoord;
 
 out vec4 fragColor;
 
-uniform sampler2D diffTex;
-uniform int drawGlyphBounds;
-
 uniform uint opts1;
-// uniform float spaceAdv = 1;
-// uniform float lineHeight = 1;
+uniform sampler2D diffTex;
 
 const uint opts1_bgColorMask = 1<<0;
 const uint opts1_underlineMask = 1<<1;
@@ -59,44 +55,14 @@ bool hasOpts(uint mask)
     return (opts1&mask) != 0;
 }
 
-// vec2 ScreenPosToGridPos(vec2 pos)
-// {
-// 	return vec2(floor(pos.x / spaceAdv), floor(pos.y / lineHeight));
-// }
-
-// vec4 GridPosToCellStartEnd(vec2 gridPos)
-// {
-// 	vec4 startEnd;
-//     startEnd.x = gridPos.x * spaceAdv;
-//     startEnd.y = gridPos.y * lineHeight;
-//     startEnd.z = startEnd.x + spaceAdv;
-//     startEnd.w = startEnd.y + lineHeight;
-//     return startEnd;
-// }
-
 void main()
 {
-    if (hasOpts(opts1_bgColorMask) && v2fUV0 == vec2(-1,-1))
+    if (hasOpts(opts1_bgColorMask) && v2fUV0 == vec2(-1, -1))
     {
-        // vec2 gridPos = ScreenPosToGridPos(v2fModelPos.xy);
-        // vec4 startEnd = GridPosToCellStartEnd(gridPos);
-        //if (gl_FragCoord.x > startEnd.x && gl_FragCoord.x < startEnd.z)
-        {
-            fragColor = v2fColor;
-        }
-
+        fragColor = v2fColor;
         return;
     }
 
     vec4 texColor = texelFetch(diffTex, ivec2(v2fUV0), 0);
-    if (texColor.r == 0)
-    {
-        if (drawGlyphBounds != 0)
-        {
-            fragColor = vec4(0,1,0,0.25);
-        }
-        return;
-    }
-
     fragColor = vec4(v2fColor.rgb, texColor.r*v2fColor.a);
 }
